841d966d4ce9bcc1c2fb807b388d358feb894279
FARHAN MUZHAFFAR TIRAS PUTRA <96176429+Hanhan73@users.noreply.github.com>
2026-06-03 21:44:08 +0700

    Update show.blade.php

--- aspapi/resources/views/member/seminar/show.blade.php
+++ aspapi/resources/views/member/seminar/show.blade.php
@@ -309,9 +309,9 @@
                     No. Sertifikat: <span class="font-mono font-semibold text-navy">{{ $enrollment->certificate->certificate_number }}</span>
                 </p>
                 <div class="flex items-center justify-center gap-3">
-                    <a style="color: black;" href="{{ route('member.seminar.certificate', $enrollment->certificate) }}"
+                    <a href="{{ route('member.seminar.certificate', $enrollment->certificate) }}"
                        target="_blank"
-                       class="inline-flex items-center gap-2 px-6 py-2.5 bg-green-600 text-white text-sm font-bold rounded-lg hover:bg-green-700 transition">
+                       class="inline-flex items-center gap-2 px-6 py-2.5 bg-green-600 text-black text-sm font-bold rounded-lg hover:bg-green-700 transition">
                         <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                   d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"/>
