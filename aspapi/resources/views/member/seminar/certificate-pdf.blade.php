66f2f17390ed7fbd62d24ef425d9214d4c6aa6e9
FARHAN MUZHAFFAR TIRAS PUTRA <96176429+Hanhan73@users.noreply.github.com>
2026-06-04 11:44:45 +0700

    oswald

--- aspapi/resources/views/member/seminar/certificate-pdf.blade.php
+++ aspapi/resources/views/member/seminar/certificate-pdf.blade.php
@@ -4,10 +4,10 @@
 <meta charset="UTF-8">
 <style>
 @font-face {
-    font-family: 'Anton';
+    font-family: 'Oswald';
     font-style: normal;
-    font-weight: normal;
-    src: url('{{ storage_path('fonts/Anton-Regular.ttf') }}') format('truetype');
+    font-weight: bold;
+    src: url('{{ storage_path('fonts/Oswald-Bold.ttf') }}') format('truetype');
 }
 
     @page {
@@ -55,9 +55,9 @@
     top: 88mm;
     left: 55mm;
     right: 20mm;
-    font-size: 36pt;
-    font-weight: normal;
-    font-family: 'Anton', sans-serif;
+    font-size: 28pt;
+    font-weight: bold;
+    font-family: 'Oswald', sans-serif;
     color: #38B6FF;
     line-height: 1.2;
     text-align: left;
